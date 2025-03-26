dbaca371fa8ab5c1eabefcca88e4194fd8507131
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2025-03-26 16:17:57 +0100

    fix: fix portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,10 +10,11 @@ export class PortfolioComponent {
   images = [
     'https://placehold.co/600x1000/yellow/white',
     'https://placehold.co/600x900/orange/white',
-    'https://placehold.co/600x980/blue/white',
+    'https://placehold.co/600x1200/blue/white',
     'https://placehold.co/700x1000/green/white',
-    'https://placehold.co/600x900',
     'https://placehold.co/600x900/orange/white',
+    'https://placehold.co/600x1200/orange/white',
     'https://placehold.co/600x900/green/white',
+    'https://placehold.co/600x1200/blue/white',
   ];
 }
