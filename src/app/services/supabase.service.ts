bf0ae38932ce7a765afa3a4f6a65289887bd65d4
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2025-03-27 12:56:24 +0100

    fix: dev environment

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +9,7 @@ export class SupabaseService {
   private supabase: SupabaseClient;
 
   constructor() {
-    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+    this.supabase = createClient(environment.supabaseUrl!, environment.supabaseKey!);
   }
 
   async getImages(sub: string): Promise<string[]> {
