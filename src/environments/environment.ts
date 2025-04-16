5768daf043d497607c13896fa905fbffe440b688
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2025-04-16 19:14:53 +0200

    revert builder ^^

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
     production: true,
     SUPABASE_URL: '',
     SUPABASE_KEY: '',
-    EMAILJS_SERVICE_ID: process.env['NG_APP_EMAILJS_SERVICE_ID'] || '',
-    EMAILJS_TEMPLATE_ID: process.env['NG_APP_EMAILJS_TEMPLATE_ID'] || '',
-    EMAILJS_PUBLIC_KEY: process.env['NG_APP_EMAILJS_PUBLIC_KEY'] || ''
+    EMAILJS_SERVICE_ID: 'service_uiznnqf',
+    EMAILJS_TEMPLATE_ID: 'template_a4n64qh',
+    EMAILJS_PUBLIC_KEY: '9kWXZ91fh0qbchxBC'
 };
 
 
