f02da595f11abccaaa70abbc3b2b81af2905df6c
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2025-04-16 17:06:00 +0200

    fix: trying default empty variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = { 
     production: true,
-    SUPABASE_URL: process.env['SUPABASE_URL'],
-    SUPABASE_KEY: process.env['SUPABASE_KEY'],
-    EMAILJS_SERVICE_ID: process.env['EMAILJS_SERVICE_ID'],
-    EMAILJS_TEMPLATE_ID: process.env['EMAILJS_TEMPLATE_ID'],
-    EMAILJS_PUBLIC_KEY: process.env['EMAILJS_PUBLIC_KEY']
+    SUPABASE_URL: process.env['SUPABASE_URL'] ||'',
+    SUPABASE_KEY: process.env['SUPABASE_KEY'] ||'',
+    EMAILJS_SERVICE_ID: process.env['EMAILJS_SERVICE_ID'] ||'',
+    EMAILJS_TEMPLATE_ID: process.env['EMAILJS_TEMPLATE_ID'] ||'',
+    EMAILJS_PUBLIC_KEY: process.env['EMAILJS_PUBLIC_KEY'] ||''
 };
 
 
